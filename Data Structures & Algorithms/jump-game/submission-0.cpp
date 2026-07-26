50153b0c8567d646975edfcad999ce2064aa7fb5
mr0crappy <74888386+mr0crappy@users.noreply.github.com>
2026-07-27 01:06:11 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int curr = 0;
+        for(int i = 0; i<nums.size(); i++){
+            if(i>curr)
+                return false;
+            curr = max(curr, i+nums[i]);
+        }
+        return true;
+    }
+};
