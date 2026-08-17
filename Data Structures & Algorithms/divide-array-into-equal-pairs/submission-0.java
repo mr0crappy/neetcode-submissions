e3df4a596bb4a0fdf169c57bdcaebebb17f84b9e
mr0crappy <74888386+mr0crappy@users.noreply.github.com>
2026-08-18 00:39:59 +0530

    Add: divide-array-into-equal-pairs - submission-0

---
+++ Data Structures & Algorithms/divide-array-into-equal-pairs/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean divideArray(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num:nums){
+            map.put(num, map.getOrDefault(num,0)+1);
+        }
+        for(int c:map.values()){
+            if(c%2!=0)
+                return false;
+        }
+        return true;
+    }
+}
